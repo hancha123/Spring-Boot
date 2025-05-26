da404e59c8cc3773404645b8582204da53a8bb88
hancha123 <i_sinclare@hanmail.net>
2025-05-26 21:12:11 +0900

    Welcome Page

---
+++ spring-boot-template-1/src/main/java/egov/SpringBootTemplate1Application.java
@@ -0,0 +1,13 @@
+package egov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTemplate1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTemplate1Application.class, args);
+	}
+
+}
